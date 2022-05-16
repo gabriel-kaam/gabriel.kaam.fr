8aebd1b8fce4eefb1cdb3631fc942301ec759323
Gabriel Kaam <gabriel@kaam.fr>
2022-05-16 21:30:59 +0200

    font: Open Sans Dark Mode

--- index.php
+++ index.php
@@ -8,8 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="This is Gabriel Kaam's official webpage. Happy ?" />
     <meta name="og:image" content="thumb.png"/>
+    <meta name="color-scheme" content="dark light">
+
     <title>Gabriel Kaam - <?= date('Y') ?></title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="style.css?49-3" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
   </head>
 
   <body>
@@ -17,13 +23,16 @@
       <p><i>&nbsp;</i><i>&nbsp;</i></p>
       <h1><a href="https://gabriel.kaam.fr">gabriel kaam</a></h1>
       <h2><a target="_blank" href="https://knr.paris" title="Site officiel de l'agence digitale KNR">KNR Agency</a></h2>
-      <h3><a target="_blank" href="https://www.modalova.fr" title="Mon nouveau projet: Modalova">modalova</a></h2>
+      <h3><a target="_blank" href="https://www.modalova.com" title="Mon nouveau projet: Modalova">modalova</a></h2>
       <div><a target="_blank" href="mailto:gabriel@kaam.fr">mail me</a></div>
       <div class="bar">#</div>
     </div>
 
     <div id="barz"><a
-      class="sprite sprite-1" target="_blank" href="https://www.linkedin.com/in/gabriel-kaam/" title="Consulter le profil Linkedin de Gabriel Kaam"></a><a
+      class="sprite sprite-1"
+      target="_blank"
+      href="https://www.linkedin.com/in/gabriel-kaam/"
+      title="Consulter le profil Linkedin de Gabriel Kaam"></a><a
       ></div>
 
     <script type="text/javascript">
